a4339b6d422f3306f32ef41e6991bbb7f72dd76c
dtzr-code <derricktay17@gmail.com>
2022-03-16 12:43:36 +0800

    first

--- index.js
+++ index.js
@@ -274,7 +274,7 @@ if (process.env.NODE_ENV === "production") {
   //npm run build
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("/api/*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
